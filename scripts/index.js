130d2236ea52fe7ad8fb1e1583e2a62ff56fc843
anasteysha0000 <Anastacia.Perevertkina@yandex.ru>
2024-03-25 21:57:23 +0300

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,33 @@
 // @todo: Темплейт карточки
+const placesElement = document.querySelector('.places__list');
+
+const cardDeleteButton = (cardElement)=>{
+    return cardElement.remove();
+  }
+
+function getTemp(element){
+    return [element.name, element.link, element.name];
+}
+function getCard(initialCard, cardDeleteButton) {
+    const card = document.querySelector('#card-template').content;
+
+    const cardElement = card.querySelector('.places__item').cloneNode(true);
+    const [cardName, cardLink, cardTitle] = getTemp(initialCard);
+    cardElement.querySelector('.card__image').src = cardLink;
+    cardElement.querySelector('.card__image').alt = cardName;
+    cardElement.querySelector('.card__title').textContent = cardTitle;
+
+    const buttonDelete = cardElement.querySelector('.card__delete-button');
+    buttonDelete.addEventListener('click', ()=> {
+        cardDeleteButton(cardElement);
+        })
+
+    return cardElement
+  }
+
+initialCards.forEach((element) =>{
+    placesElement.append(getCard(element, cardDeleteButton))
+  });
 
 // @todo: DOM узлы
 
